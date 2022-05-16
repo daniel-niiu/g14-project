7ad22e1ddb8a283fa5a864ca82cd5b3a187daad4
SiawWei <102763130@students.swinburne.edu.my>
2022-05-16 15:59:50 +0800

    update db connection

--- db/dbconnection.php
+++ db/dbconnection.php
@@ -11,4 +11,34 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+return $conn;
+
+
+function isMainPageLoggedIn()
+{   
+    
+    if(isset($_SESSION["status"]) && $_SESSION["status"] === "T")
+    {
+        return true;
+    } 
+    else
+    {
+        $_SESSION["status"] = "F";
+    }
+}
+
+function isLoggedIn()
+{   
+    
+    if(isset($_SESSION["status"]) && $_SESSION["status"] === "T")
+    {
+        return true;
+    }
+    else
+    {
+        $_SESSION["status"] = "F";
+        header("Location: ../index.php"); 
+    }
+}
 ?>
