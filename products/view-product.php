3f2cc2258913efe3779dbb21cf118ce93ac505e2
usama2017 <55685199+usama2017@users.noreply.github.com>
2022-09-19 07:41:34 +0500

    Update view-product.php

--- products/view-product.php
+++ products/view-product.php
@@ -42,7 +42,284 @@ isLoggedIn();
 			  </a>
 			</li>
 			<li>
+			  <div class="flex items-center"><?php
+include "../db/dbconnection.php";    
+isLoggedIn();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://kit.fontawesome.com/b41521ee1f.js"></script>
+	<script>
+		if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	</script>
+	<link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.3/dist/flowbite.min.css" />
+	<link rel="stylesheet" href="../styles/style.css">
+	<link rel="icon" type="image/x-icon" href="../images/logo.ico">
+	<title>Tze Yin Membership Management Portal</title>
+</head>
+
+<body class="dark:bg-gray-900">
+	<?php
+		include('../templates/header.php');
+	?>	
+	
+	<div class="container flex flex-wrap mx-auto">
+		
+	<?php
+		include('../templates/product-aside.php');
+	?>
+	
+	<div class="mx-auto">
+		<nav class="flex mb-4" aria-label="Breadcrumb">
+		  <ol class="inline-flex items-center space-x-1 md:space-x-3">
+			<li class="inline-flex items-center">
+			  <a href="../index.php" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+				<svg class="mr-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+				Home
+			  </a>
+			</li>
+			<li>
 			  <div class="flex items-center">
+				<svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+				<a href="search-product.php?name=product&aside=search-product" class="ml-1 text-sm font-medium text-gray-700 md:ml-2 dark:text-gray-400">Search Product</a>
+			  </div>
+			</li>
+			<li>
+			  <div class="flex items-center">
+				<svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+				<p class="ml-1 text-sm font-medium text-gray-700 md:ml-2 dark:text-gray-400">View Product</p>
+			  </div>
+			</li>
+		  </ol>
+		</nav>
+	
+					<?php  
+					$M_ID = $_GET['Id']; 
+		    	$sql = "SELECT * FROM product WHERE product_id = '".$M_ID."'"; 	  
+		    	$result = $conn->query($sql);  
+					if (mysqli_num_rows($result) > 0) {
+		  			// output data of each row
+		  				$row = mysqli_fetch_array($result); 
+					?>
+		<div>
+			<div class="container flex flex-wrap justify-between items-center mx-auto">
+				<h2 class="flex items-center mb-1 text-xl font-bold text-gray-900 dark:text-white">View Product</h2>
+				<div class="button">
+					<a href="edit-product.php?name=product&Id=<?php echo $row['product_id']; ?>">
+						<button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+							<i class="fa-solid fa-pencil"></i>&nbsp; Edit Product
+						</button>
+					</a>
+					
+					<button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" data-modal-toggle="popup-modal">
+						<i class="fa-solid fa-trash-can"></i>&nbsp; Delete Product
+					</button> 
+					<div id="delete-popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+						<div class="relative w-full h-full max-w-md p-4 md:h-auto">
+							<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+								<button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="delete-popup-modal">
+									<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
+								</button>
+								<div class="p-6 text-center">
+									<svg class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+									<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this record?</h3>
+									<a href="../php/product.php?Id=<?php echo $row['product_id'];?>&method=delete"><button data-modal-toggle="delete-popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+										Yes, I'm sure
+									</button></a>
+									<button data-modal-toggle="delete-popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div> 
+			<hr class="border-gray-300 dark:border-gray-600 my-3"/>
+			
+			<form>
+				<div class="grid xl:grid-cols-2 xl:gap-6">
+					<div class="relative z-0 w-full mb-6 group">
+						<label for="id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Product ID</label>
+						<input type="text" id="id" name="id" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400" value="<?php echo $row['product_id']; ?>" disabled readonly>
+					</div>
+				 	<div class="relative z-0 w-full mb-6 group">
+						<label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Product Status</label>
+						<input type="text" id="status" name="status" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400" value="<?php echo $row['product_status']; ?>"disabled readonly>
+					 </div>
+				 </div>
+				<div class="grid xl:grid-cols-2 xl:gap-6">
+					<div class="relative z-0 w-full mb-6 group">
+						<label for="eng" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Product Name(English)</label>
+						<input type="text" id="eng" name="eng" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400" value="<?php echo $row['product_eng_name']; ?>" disabled readonly>
+					</div>
+				 	<div class="relative z-0 w-full mb-6 group">
+						<label for="chi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Product Name(Chinese)</label>
+						<input type="text" id="chi" name="chi" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400" value="<?php echo $row['product_chi_name']; ?>"disabled readonly>
+					 </div>
+				 </div>
+				<div class="grid xl:grid-cols-2 xl:gap-6">
+					<div class="relative z-0 w-full mb-6 group">
+						<label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Unit Price</label>
+						<div class="flex">
+							 <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">RM</span>
+							 <input type="text" id="price" name="price" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-none rounded-r-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400" value="<?php echo $row['unit_price']; ?>" disabled readonly>
+						</div>
+					 </div>
+					<div class="relative z-0 w-full mb-6 group">
+						<label for="user" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Recorded By</label>
+						<input type="text" id="user" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400" value="<?php echo $row['username']; ?>" disabled readonly>
+					</div>
+				</div>
+				<div class="grid xl:grid-cols-2 xl:gap-6">
+					<div class="relative z-0 w-full mb-6 group">
+						<label for="record-date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Recorded On</label>
+						<input type="text" id="record-date" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400" value="<?php echo $row['username']; ?>" disabled readonly>
+					</div>
+				</div>
+				<div class="relative z-0 w-full mb-6 group">
+					<label for="remarks" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remarks</label>
+					<textarea id="remarks" rows="3" cols="125" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400" disabled readonly><?php echo $row['remarks']; ?></textarea>
+				</div>
+			</form>
+			<?php
+			}
+			?>
+			
+			<div class="container flex flex-wrap justify-between items-center mx-auto pt-4">
+				<h2 class="flex items-center mb-1 text-xl font-bold text-gray-900 dark:text-white">View Product Stocks</h2>
+				<div class="button">
+					<a href="create-product-product.php?name=product&aside=product-product">
+						<button type="button" class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+							<i class="fa-solid fa-plus-minus"></i>&nbsp; Stock In/Out
+						</button>
+					</a>
+				</div>
+			</div>
+			
+			<hr class="border-gray-300 dark:border-gray-600 my-3"/>
+			
+			<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+				<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+					<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+						<tr>
+							<th scope="col" class="px-6 py-3">Receipt date</th>
+							<th scope="col" class="px-6 py-3">Receipt no</th>
+							<th scope="col" class="px-6 py-3">Stock In</th>
+							<th scope="col" class="px-6 py-3">Stock Out</th>
+							<th scope="col" class="px-6 py-3">Balance</th>
+							<th scope="col" class="px-6 py-3">
+								<span class="sr-only">Edit</span>
+							</th>
+						</tr>
+					</thead>
+					<tbody> 
+                    	<?php  	 
+			            if($query->num_rows > 0){
+			                while($row = $query->fetch_assoc()){
+			            ?>
+			                 <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+			                 	<th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+			                    	<a href="view-product-product.php?name=transaction&Id=<?php echo $row["product_id"]; ?>&receiptNum=<?php echo $row['receipt_num']; ?>&receiptDate=<?php echo $row['receipt_date'];?>" class="dark:hover:text-blue-500 md:hover:text-blue-700"><?php echo $row["receipt_date"]; ?></a>
+			                    </th> 
+			                    <td class="px-6 py-4"><?php echo $row["receipt_num"]; ?></td>
+			                    <td class="px-6 py-4"><?php echo $row["product_eng_name"]; ?></td>
+			                    <td class="px-6 py-4"><?php echo $row["product_chi_name"]; ?></td>
+			                    <td class="px-6 py-4"><?php echo $row["receipt_amount"]; ?></td>
+			                    <td class="px-6 py-4 text-right">
+			                    	<a href="edit-product-product.php?name=transaction&Id=<?php echo $row["product_id"]; ?>&receiptNum=<?php echo $row['receipt_num']; ?>&receiptDate=<?php echo $row['receipt_date'];?>" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+			                    </td>
+			                </tr>
+			            <?php
+			                } 
+			            }
+		                else{ 
+		                    echo '<tr><td colspan="5">No records found...</td></tr>'; 
+		                } 
+                    	?> 
+					</tbody>
+				</table>
+			</div>
+			
+			<nav aria-label="Page navigation" class="mt-6 mb-2 text-center"> 
+			    <?php echo $pagination->createLinks(); ?>   
+			</nav>		
+		</div>
+		<?php
+		 
+		?>
+	</div>
+	</div>
+	
+	<!--Edit Product: Success Toast-->
+	<div id="toast-success" class="hidden flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 fixed bottom-5 left-5" role="alert">
+		<div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+			<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+		</div>
+		<div class="ml-3 text-sm font-normal">Product modified successfully.</div>
+		<button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
+			<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+		</button>
+	</div>
+	
+	<hr class="border-gray-300 dark:border-gray-600 mt-4"/>
+	
+	<footer>
+		<p class="text-center text-xs font-normal text-gray-500 dark:text-gray-400 my-4">Disclaimer: This is a student work in progress for SWE40001/SWE40002 Software Engineering Project A/B of Swinburne University of Technology, Sarawak (2022).</p>
+	</footer>
+	
+	<script src="https://unpkg.com/flowbite@1.4.3/dist/flowbite.js"></script>
+	
+	<script>
+		var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+		var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+		// Change the icons inside the button based on previous settings
+		if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+			themeToggleLightIcon.classList.remove('hidden');
+		} else {
+			themeToggleDarkIcon.classList.remove('hidden');
+		}
+
+		var themeToggleBtn = document.getElementById('theme-toggle');
+
+		themeToggleBtn.addEventListener('click', function() {
+
+			// toggle icons inside button
+			themeToggleDarkIcon.classList.toggle('hidden');
+			themeToggleLightIcon.classList.toggle('hidden');
+
+			// if set via local storage previously
+			if (localStorage.getItem('color-theme')) {
+				if (localStorage.getItem('color-theme') === 'light') {
+					document.documentElement.classList.add('dark');
+					localStorage.setItem('color-theme', 'dark');
+				} else {
+					document.documentElement.classList.remove('dark');
+					localStorage.setItem('color-theme', 'light');
+				}
+
+			// if NOT set via local storage previously
+			} else {
+				if (document.documentElement.classList.contains('dark')) {
+					document.documentElement.classList.remove('dark');
+					localStorage.setItem('color-theme', 'light');
+				} else {
+					document.documentElement.classList.add('dark');
+					localStorage.setItem('color-theme', 'dark');
+				}
+			}
+
+		});
+	</script>
+</body>
+</html>
+
 				<svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
 				<a href="search-product.php?name=product&aside=search-product" class="ml-1 text-sm font-medium text-gray-700 md:ml-2 dark:text-gray-400">Search Product</a>
 			  </div>
