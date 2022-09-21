1cc6ef2dd0e5ee448c003ec015375c3ed76038aa
ramishbk647 <92035219+ramishbk647@users.noreply.github.com>
2022-09-21 08:17:31 +0500

    PHP Sotck In?Stock Out - v1.3

--- stocks/stock-in.php
+++ stocks/stock-in.php
@@ -57,7 +57,7 @@ isLoggedIn();
 			
 
 			
-			<form method="post" action="../php/member.php?method=add">
+			<form method="post" action="../php/stock.php?method=add">
 				<div class="grid xl:grid-cols-2 xl:gap-6">
 					<div class="relative z-0 w-full mb-6 group">
 						<label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Product Name*</label>
@@ -69,7 +69,7 @@ isLoggedIn();
   							<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
     							<svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
   							</div>
-  							<input datepicker datepicker-format="dd/mm/yyyy" datepicker-buttons type="text" id="date" name="date" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="dd/mm/yyyy">
+  							<input datepicker datepicker-format="yyyy/mm/dd" datepicker-buttons type="text" id="date" name="date" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="yyyy/mm/dd" required>
 						</div>
 					</div>
 				</div>
