fefa6e370ff8c6736a2f422e4305d0b00f923070
SiawWei <102763130@students.swinburne.edu.my>
2022-10-21 22:59:27 +0800

    update tablet transaction info

--- transactions/create-tablet-transaction.php
+++ transactions/create-tablet-transaction.php
@@ -90,8 +90,21 @@ isLoggedIn();
 				<div class="grid xl:grid-cols-2 xl:gap-6">
 					<div class="relative z-0 w-full mb-6 group">
 						<label for="id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"><?php echo $transaction['tablet-id']; ?></label>
-						<input type="text" id="id" name="t_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="<?php echo $transaction['tablet-id']; ?>" value="<?php echo $_GET['Id']; ?>" disabled> 
+						<?php 
+							if($_GET['Id'] != null)
+							{ 
+						?>
+						<input type="text" id="id" name="t_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="T000" value="<?php echo $_GET['Id']; ?>" disabled> 
 						<input type="hidden" name="id"value="<?php echo $_GET['Id']; ?>"> 
+						<?php
+						 	}
+						 	else
+						 	{
+						 ?>
+						<input type="text" id="id" name="id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="T000">  
+						 <?php
+						 	} 
+						?>
 					</div>
 					<div class="relative z-0 w-full mb-6 group">
 						<label for="receipt" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"><?php echo $form['receipt-num']; ?></label>
