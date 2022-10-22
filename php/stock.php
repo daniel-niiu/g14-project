edaa95343b8e05f181d4bdc674a2db58ceaf0941
ramishbk647 <92035219+ramishbk647@users.noreply.github.com>
2022-10-22 19:39:22 +0500

    Add files via upload

--- php/stock.php
+++ php/stock.php
@@ -34,32 +34,31 @@ if($method === "add")
 
 }
 
-if($method === "edit")
+if($method === "update")
 {
 	
     global $conn;
  
-    $Id = $_POST["pID"];
+	$name = $_POST["name"];  
     $date = $_POST["date"];
 	$summary = $_POST["summary"];
     $receiptno = $_POST["receipt"];
 	$stockin = $_POST["stock-in"];
-	$stockout = $_POST["stock-out"];
 	$balance = $_POST["balance"];
     $remarks = $_POST["remarks"];
 
-   	$sql = "UPDATE STOCK SET stock_summary = '$summary', stock_in = '$stockin', stock_out = '$stockout', balance_left = '$balance', remarks = '$remarks', recordedBy = '".$_SESSION['name']."', recordedOn = '".date("Y-m-d h:i:s")."'
-		WHERE product_id = '$Id' AND receipt_no = '$receiptno' AND stock_date = '$date'";  
+   	$sql = "UPDATE STOCK SET stock_summary = '$summary', stock_in = '$stockin', balance_left = '$balance', remarks = '$remarks', recordedBy = '".$_SESSION['name']."', recordedOn = '".date("Y-m-d h:i:s")."'
+		WHERE product_name = '$name' AND receipt_no = '$receiptno' AND stock_date = '$date'";  
 	
     if (!mysqli_query($conn,$sql)) {
 		
-        header("Location: ../products/view-product.php?name=product&Id=$Id");
+        header("Location: ../products/view-stock.php?name=stockin&name=$name");
     
 	} 
 	
     else {
 		
-        header("Location: ../products/view-product.php?name=product&Id=$Id");
+        header("Location: ../products/view-stock.php?name=stockin&name=$name");
     
 	}  
 	
