4bcd466629f73f41ff65c9d9715cd3ab57e235cc
SiawWei <102763130@students.swinburne.edu.my>
2022-11-24 22:35:20 +0800

    asd
    asd

--- analytics/stocks-report-data.php
+++ analytics/stocks-report-data.php
@@ -6,8 +6,8 @@ if(isset($_POST['start-date']) && isset($_POST['end-date']) && !empty($_POST['st
     //echo '<pre>'.print_r($_POST,true).'</pre>';
     $sd = explode('/',$_POST['start-date']);
     $ed = explode('/',$_POST['end-date']);
-    $startDate = $sd[2].'-'.$sd[1].'-'.$sd[0];
-    $endDate = $ed[2].'-'.$ed[1].'-'.$ed[0];
+    $startDate = $sd[2].'-'.$sd[0].'-'.$sd[1];
+    $endDate = $ed[2].'-'.$ed[0].'-'.$ed[1];
     $timeDataFilter = ' WHERE (UNIX_TIMESTAMP(s.recordedOn) >= UNIX_TIMESTAMP("'.$startDate.' 00:00:00") AND UNIX_TIMESTAMP(s.recordedOn) <= UNIX_TIMESTAMP("'.$endDate.' 23:59:59")) AND (UNIX_TIMESTAMP(so.recordedOn) >= UNIX_TIMESTAMP("'.$startDate.' 00:00:00") AND UNIX_TIMESTAMP(so.recordedOn) <= UNIX_TIMESTAMP("'.$endDate.' 23:59:59")) ';
 
 }
@@ -20,7 +20,7 @@ if(isset($_POST['title']) && !empty($_POST['title'])){
     }
 
 }
-$sql = "SELECT p.product_chi_name as 'Product', sum(stock_in) as stock_in, sum(stock_out) AS stock_out FROM (stockin s JOIN stockout so ON s.product_name = so.product_name) JOIN product p ON p.product_id = s.product_name $timeDataFilter Group by s.product_name";
+$sql = "SELECT p.product_chi_name as 'Product', sum(stock_in) as stock_in, sum(stock_out) AS stock_out FROM (stockin s JOIN stockout so ON s.product_name = so.product_name) JOIN product p ON p.product_id = s.product_name $timeDataFilter Group by s.product_name"; 
 $result = $conn->query($sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
--- index.php
+++ index.php
@@ -213,7 +213,7 @@ isMainPageLoggedIn();
             				</button>
             				<div class="px-6 py-6 lg:px-8">
                 				<h3 class="mb-6 text-xl font-medium text-gray-900 dark:text-white"><?php echo $index['index-filter-title']; ?></h3>
-                				<form class="space-y-6" action="../members/member-excel.php" method="post">	
+                				<div class="space-y-6">	 
     								<div class="grid gap-6 md:grid-cols-2">
                                         <div>
                                              <input id="create-members" name="preference" type="checkbox" value="1" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" >
