c9cdb4e353aaf9de09441cac0bd55b530e6fa961
SiawWei <102763130@students.swinburne.edu.my>
2022-11-28 00:56:33 +0800

    Revert the changes with stock in and stock out

--- stocks/stock-in.php
+++ stocks/stock-in.php
@@ -60,7 +60,7 @@ isLoggedIn();
 
 				<div class="flex justify-between">
 					<button type="button" class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" data-modal-toggle="import-modal">
-						<i class="fa-solid fa-file-import"></i>&nbsp; <?php echo $import['import-tablet']; ?>
+						<i class="fa-solid fa-file-import"></i>&nbsp; <?php echo $import['import-record']; ?>
 					</button>
 					
 					<div id="import-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
