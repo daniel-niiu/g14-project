ab590d303a2b0df39aaaf55fd5b757dde5d32524
ramishbk647 <92035219+ramishbk647@users.noreply.github.com>
2022-11-29 18:43:08 +0500

    Add files via upload
    update stock analytics- bugs fixed

--- analytics/stocks-report-data.php
+++ analytics/stocks-report-data.php
@@ -6,8 +6,8 @@ if(isset($_POST['start-date']) && isset($_POST['end-date']) && !empty($_POST['st
     //echo '<pre>'.print_r($_POST,true).'</pre>';
     $sd = explode('/',$_POST['start-date']);
     $ed = explode('/',$_POST['end-date']);
-    $startDate = $sd[2].'-'.$sd[0].'-'.$sd[1];
-    $endDate = $ed[2].'-'.$ed[0].'-'.$ed[1];
+    $startDate = $sd[2].'-'.$sd[1].'-'.$sd[0];
+    $endDate = $ed[2].'-'.$ed[1].'-'.$ed[0];
     $timeDataFilter = ' WHERE (UNIX_TIMESTAMP(s.recordedOn) >= UNIX_TIMESTAMP("'.$startDate.' 00:00:00") AND UNIX_TIMESTAMP(s.recordedOn) <= UNIX_TIMESTAMP("'.$endDate.' 23:59:59")) AND (UNIX_TIMESTAMP(so.recordedOn) >= UNIX_TIMESTAMP("'.$startDate.' 00:00:00") AND UNIX_TIMESTAMP(so.recordedOn) <= UNIX_TIMESTAMP("'.$endDate.' 23:59:59")) ';
 
 }
@@ -20,11 +20,18 @@ if(isset($_POST['title']) && !empty($_POST['title'])){
     }
 
 }
-$sql = "SELECT p.product_chi_name as 'Product', sum(stock_in) as stock_in, sum(stock_out) AS stock_out FROM (stockin s JOIN stockout so ON s.product_name = so.product_name) JOIN product p ON p.product_id = s.product_name $timeDataFilter Group by s.product_name"; 
+$sql = "SELECT s.product_name AS 'Product', sum(s.stock_in) AS stock_in ,count(s.product_id) as spcount, sum(so.stock_out) AS stock_out, count(so.product_id) as sopcount FROM stockin AS s LEFT Join stockout AS so on s.product_id = so.product_id $timeDataFilter";
 $result = $conn->query($sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
-    $row = mysqli_fetch_all($result,MYSQLI_ASSOC);  
+    $rows = mysqli_fetch_all($result,MYSQLI_ASSOC);
+    foreach ($rows as $r) {
+        $row[]=[
+            'Product'=>$r['Product'],
+            'stock_in'=>($r['stock_in']/sqrt($r['spcount'])),
+            'stock_out'=>($r['stock_out']/sqrt($r['sopcount'])),
+        ];
+    }
     echo json_encode($row, true);
 }
 ?>
